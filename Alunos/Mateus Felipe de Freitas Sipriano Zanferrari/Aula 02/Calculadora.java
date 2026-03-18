975128790f9b94f3ef642b64d030296ddee10d93
MateusZanferrari <matteus.ffs@hotmail.com>
2026-03-17 21:21:42 -0300

    Atualizado o arquivo da pasta aula 02 e melhorando a calculadora na pasta da aula 03

---
+++ Alunos/Mateus Felipe de Freitas Sipriano Zanferrari/Aula 02/Calculadora.java
@@ -0,0 +1,55 @@
+
+import java.util.Scanner;
+
+public class Calculadora{
+
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int escolha;
+
+        do{
+        System.out.println("\nMENU LOJA");
+        System.out.println("1- Calcular Preço");
+        System.out.println("2- Calcular troco");
+        System.out.println("3- Sair");
+
+        escolha = scan.nextInt();
+
+        switch (escolha){
+            case 1: 
+            System.out.println("Digite a quantidade de itens:");
+            double a = scan.nextDouble();
+            System.out.println("Digite o valor do iten:");
+            double b = scan.nextDouble();
+            double precototal = calculopreco(a, b);
+            System.out.println("Preco total R$" + precototal);
+                break;
+
+            case 2: 
+            System.out.println("Digite o valor recebido:");
+            double c = scan.nextDouble();
+            System.out.println("Digite o valor total da compra:");
+            double d = scan.nextDouble();
+            double troco = calculoTroco(c, d);
+            System.out.println("Troco da Compra R$" + troco);
+                break;
+
+            case 3: 
+            System.out.println("Fechando calculadora!");
+                break;
+            
+        }
+        }while (escolha !=3);
+        
+        scan.close();
+    }   
+    
+
+    public static double calculopreco (double  a, double b){
+           
+                return a*b;} 
+
+    public static double calculoTroco (double c, double d){
+                return c-d;}
+
+}
